93aba0bf5cb6d025663cbe042bedda17fb13f885
zhaokang03 <zhaokang03@inspur.com>
2022-11-13 21:00:02 +0800

    update

---
+++ src/main/java/com/karmai/blog_back/BlogBackApplication.java
@@ -0,0 +1,13 @@
+package com.karmai.blog_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackApplication.class, args);
+    }
+
+}
