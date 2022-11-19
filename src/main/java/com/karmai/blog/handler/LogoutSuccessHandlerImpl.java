e91afd3d2af0a4fc6542d8f4fb8fc9ad3d1ea549
’zhaokang66‘ <zhaokang03@inspur.com>
2022-11-19 16:52:27 +0800

    debug

--- src/main/java/com/karmai/blog/handler/LogoutSuccessHandlerImpl.java
+++ src/main/java/com/karmai/blog/handler/LogoutSuccessHandlerImpl.java
@@ -2,6 +2,7 @@ package com.karmai.blog.handler;
 
 import cn.hutool.json.JSONUtil;
 import com.karmai.blog.entity.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,11 +18,13 @@ import java.nio.charset.StandardCharsets;
  * @Author zhaokang03
  * @Date 2022/11/15 15:39
  */
+@Slf4j
 @Component
 public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        log.info("开始注销...");
         httpServletResponse.setContentType("applcation/json");
         ServletOutputStream outStream = httpServletResponse.getOutputStream();
         outStream.write(JSONUtil.toJsonStr(Result.ok("注销成功！")).getBytes(StandardCharsets.UTF_8));
