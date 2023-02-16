bd61da46146ef1e39cc11dadffdbd2252bef4cdf
zhaokang03 <zhaokang03@inspur.com>
2023-02-16 11:37:58 +0800

    rabbitMQ改为手动签发消息

--- src/main/java/com/karmai/blog/consumer/TestConsumer.java
+++ src/main/java/com/karmai/blog/consumer/TestConsumer.java
@@ -1,11 +1,15 @@
 package com.karmai.blog.consumer;
 
 import com.karmai.blog.entity.SysMenu;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author zhaokang03
@@ -22,7 +26,10 @@ public class TestConsumer {
             )
     )
     @RabbitHandler
-    public void onListen(@Payload List<SysMenu> data) {
+    public void onListen(@Payload List<SysMenu> data, @Headers Map<String ,Object> headers, Channel channel) throws Exception{
         System.out.println(data);
+        // 如果消息的签收模式是手动
+        Long deliverTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        channel.basicAck(deliverTag,false);// 第二个参数代表是否支持批量签收
     }
 }
